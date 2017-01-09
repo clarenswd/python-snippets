4b9018d5cd118b50afe86cc782865f101c6c315f
clarens <clarens007@gmail.com>
2017-01-09 10:28:30 +1000

    general stuff

---
+++ general-stuff.py
@@ -0,0 +1,13 @@
+'''
+I don't program in python everyday, so to not forget how to write a for loop or any general stuff, I better write it here
+
+ヾ(`ヘ´)ﾉﾞ
+
+
+'''
+
+
+#FOR LOOP
+for letter in 'Python':
+   print ('Current Letter :', letter)
+
