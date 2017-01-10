8b5722420d32319d7d86511672073370fc7798ae
clarens <clarens007@gmail.com>
2017-01-10 12:41:47 +1000

    ternary operator

--- general-stuff.py
+++ general-stuff.py
@@ -11,3 +11,10 @@ I don't program in python everyday, so to not forget how to write a for loop or 
 for letter in 'Python':
    print ('Current Letter :', letter)
 
+
+
+
+#ternary Operator 
+
+
+only_one_page  =  False if holder.number_pages > 1 else True
